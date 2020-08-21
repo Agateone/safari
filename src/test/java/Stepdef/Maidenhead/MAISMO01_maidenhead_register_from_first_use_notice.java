96062f1e2fd2387737d0518775d889ee38584c1d
Agateone <jayasree.kamineni1@gmail.com>
2020-08-21 13:12:47 +0100

    sauce

--- src/test/java/Stepdef/Maidenhead/MAISMO01_maidenhead_register_from_first_use_notice.java
+++ src/test/java/Stepdef/Maidenhead/MAISMO01_maidenhead_register_from_first_use_notice.java
@@ -4,9 +4,10 @@ package Stepdef.Maidenhead;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.Test;
+
 import cucumber.api.java.en.Given;
+
 import java.net.URL;
 
 
@@ -18,14 +19,14 @@ public class MAISMO01_maidenhead_register_from_first_use_notice {
 	
 	//--------------------POPSMO01-----------------------
 	//Given I am a user of Axate and I am on the registration page through maidenhead FUN 
-	@Given("^I am a user of Axate and I am on the registration page through maidenhead FUN")
+	@Given("I am a user of Axate and I am on the registration page through maidenhead FUN")
 	@Test(priority=1)
-	public void i_am_a_user_of_Axate_and_I_am_on_the_registration_page_through_maidenhead_FUN() throws Throwable {
+	public void i_am_a_user_of_Axate_and_I_am_on_the_registration_page_through_maidenhead_FUN()throws MalformedURLException{		
 		
-		DesiredCapabilities caps = DesiredCapabilities.safari();
-	    caps.setCapability("platform", "macOS 10.15.5");
-	    caps.setCapability("version", "latest");
-	    WebDriver driver = new RemoteWebDriver(new URL(URL),caps);
+		 DesiredCapabilities caps = DesiredCapabilities.chrome();
+		    caps.setCapability("platform", "Windows 10");
+		    caps.setCapability("version", "latest");
+		    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
 
 					driver.get("https://popbitch.com");					
 				

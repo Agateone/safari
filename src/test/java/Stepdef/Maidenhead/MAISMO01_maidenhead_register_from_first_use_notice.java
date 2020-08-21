7113e529ebe192feb6f91961cdbcb9328f24a8f6
Agateone <jayasree.kamineni1@gmail.com>
2020-08-21 01:42:23 +0100

    initial

--- src/test/java/Stepdef/Maidenhead/MAISMO01_maidenhead_register_from_first_use_notice.java
+++ src/test/java/Stepdef/Maidenhead/MAISMO01_maidenhead_register_from_first_use_notice.java
@@ -1,25 +1,12 @@
 package Stepdef.Maidenhead;
 
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import Elements.Wallet_Elements;
-import Elements.Finish_Notice_elements;
-import Elements.Popbitch_First_Use_Notice_Elements;
-import Elements.Register_Page_Elements1;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+
 
 public class MAISMO01_maidenhead_register_from_first_use_notice {
 	//Initiate driver

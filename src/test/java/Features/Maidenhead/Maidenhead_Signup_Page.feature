451fc9af86a79f365c2a163bc3f3d025578c4a59
Agateone <jayasree.kamineni1@gmail.com>
2020-08-21 12:16:20 +0100

    sauce

--- src/test/java/Features/Maidenhead/Maidenhead_Signup_Page.feature
+++ src/test/java/Features/Maidenhead/Maidenhead_Signup_Page.feature
@@ -5,7 +5,7 @@ Feature: Register to Axate
 #****
 #POPSMO01 
 Scenario: Successful signup from maidenhead FUN  
-Given I am a user of Axate and I am on the registration page through maidenhead FUN "browser"
+Given I am a user of Axate and I am on the registration page through maidenhead FUN
 When I enter all the required details on step one and click on continue
 And top up with a valid card in steptwo with one pound and click on continue
 Then I get a funded axate wallet with one pound in it
--- src/test/java/Stepdef/Maidenhead/MAISMO01_maidenhead_register_from_first_use_notice.java
+++ src/test/java/Stepdef/Maidenhead/MAISMO01_maidenhead_register_from_first_use_notice.java
@@ -15,20 +15,17 @@ public class MAISMO01_maidenhead_register_from_first_use_notice {
 	public static final String USERNAME = "axate2020";
 	  public static final String ACCESS_KEY = "2e99804e-89a3-4e7e-9d56-370719cb578a";
 	  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
-	 
-	
-	
 	
 	//--------------------POPSMO01-----------------------
 	//Given I am a user of Axate and I am on the registration page through maidenhead FUN 
-	@Given("^I am a user of Axate and I am on the registration page through maidenhead FUN \"([^\"]*)\"$")
+	@Given("^I am a user of Axate and I am on the registration page through maidenhead FUN")
 	@Test(priority=1)
 	public void i_am_a_user_of_Axate_and_I_am_on_the_registration_page_through_maidenhead_FUN() throws Throwable {
 		
 		DesiredCapabilities caps = DesiredCapabilities.safari();
 	    caps.setCapability("platform", "macOS 10.15.5");
 	    caps.setCapability("version", "latest");
-	    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+	    WebDriver driver = new RemoteWebDriver(new URL(URL),caps);
 				driver= new SafariDriver();
 				driver.manage().window().maximize();
 				try {
